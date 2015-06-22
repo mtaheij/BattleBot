83fdbb14db92ba4cc14edf8b40d84dd1ef1fa628
Zach <zbfiorito@gmail.com>
2015-06-22 18:36:50 -0500

    Maybe this time

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -5,7 +5,7 @@ from cloudbot import hook
 #BattlePluginsAPI key
 bp_key = bot.config.get("api_keys", {}).get("battleplugins_key", {})
 #BattlePlugins shortner endpoint
-bp_shorten = https://api.battleplugins.com/v1/shorturls
+bp_shorten = "https://api.battleplugins.com/v1/shorturls"
 
 @hook.command()
 def shorten(text):
