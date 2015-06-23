4b6e36f5d1188eb58ff693082aaa2051ceb70772
Zach443 <zbfiorito@gmail.com>
2015-06-22 19:01:24 -0500

    Stuff

---
+++ plugins/zach.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from cloudbot import hook
+
+#BattlePluginsAPI key
+bp_key = bot.config.get("api_keys", {}).get("battleplugins_key", {})
+#BattlePlugins shortner endpoint
+bp_shorten = "https://api.battleplugins.com/v1/shorturls"
+
+@hook.command()
+def shorten(text):
+    #url = args[0]
+
+    #payload = {'_key': bp_key, 'url': url}
+    #r = requests.post(bp_shorten, params=payload)
+
+    #message = r.text()
+
+    return "https://bplug.in/"
