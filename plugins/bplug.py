c083de459a06801caa5f7ad0e5d2e2259786f516
Zach443 <zbfiorito@gmail.com>
2015-06-22 21:11:00 -0500

    I hate Python with all my heart

---
+++ plugins/bplug.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from cloudbot import hook
+
+@hook.command()
+def bplug(text, bot):
+    #BattlePlugins shortner endpoint
+    bp_shorten = "https://api.battleplugins.com/v1/shorturls"
+    #BattlePluginsAPI key
+    bp_key = bot.config.get("api_keys", {}).get("battleplugins_key", {})
+
+    payload = {'_key': bp_key, 'url': text}
+    r = requests.post(bp_shorten, params=payload)
+
+    response = r.json()
+
+    if response.get("success"):
+        return response["success"]["message"]
+    else:
+        return "Failed to shorten URL: " + response["error"]["message"
